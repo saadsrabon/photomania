8c76fb94fecf6261213c10c9e6be3716a8d9138a
Saad Srabon <saadsrabon2@gmail.com>
2024-04-04 15:44:46 +0600

    first route handler added

--- app/api/photos/route.js
+++ app/api/photos/route.js
@@ -1,6 +1,6 @@
 import { getAllPhotos } from "@/app/lib/image_data";
 import { NextResponse } from "next/server";
-
+// Functions to get all photos
 export async function GET(){
     
     const photos = await getAllPhotos();
